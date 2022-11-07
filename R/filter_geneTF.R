442af3131d8aebb97b3fbe5d7bc688dd617ca81b
xim2016 <xj_ma@yahoo.com>
2022-11-06 22:22:00 -0500

    update commit

--- R/filter_geneTF.R
+++ R/filter_geneTF.R
@@ -1,5 +1,5 @@
 #' Filter geneTF prior based on the gene list user provided
-#' @param geneTF_matrix geneTF prior
+#' @param geneTF_prior geneTF prior
 #' @param gene_list gene list user provided
 #' @param TF_upbound the upper bound of percentage of TF targeted genes over all genes. The default is 80\%
 #' @param TF_lowbound  the lower bound of number of TF targeted genes. The defalut is 10
@@ -7,13 +7,13 @@
 #' @return geneTF matrix
 #' @export
 
-filter_geneTF <- function(geneTF_matrix, gene_list, TF_upbound=0.8, TF_lowbound=10, gene_lowbound=5){
+filter_geneTF <- function(geneTF_prior, gene_list, TF_upbound=0.8, TF_lowbound=10, gene_lowbound=5){
 
-  gene_comm <- intersect(rownames(geneTF_matrix), gene_list)
+  gene_comm <- intersect(rownames(geneTF_prior), gene_list)
 
-  # TF_comm <- intersect(colnames(geneTF_matrix), gene_list)
+  # TF_comm <- intersect(colnames(geneTF_prior), gene_list)
 
-  df_geneTF <- geneTF_matrix[gene_comm,]
+  df_geneTF <- geneTF_prior[gene_comm,]
 
   df_geneTF_1 <- filter_gene(df_geneTF, gene_lowbound)
 
@@ -26,11 +26,11 @@ filter_geneTF <- function(geneTF_matrix, gene_list, TF_upbound=0.8, TF_lowbound=
   return(df_geneTF_4)
 }
 
-filter_gene <- function(geneTF_matrix, gene_lowbound){
-  return(geneTF_matrix[rowSums(geneTF_matrix != 0) > gene_lowbound,])
+filter_gene <- function(geneTF_prior, gene_lowbound){
+  return(geneTF_prior[rowSums(geneTF_prior != 0) > gene_lowbound,])
 }
 
-filter_TF <- function(geneTF_matrix, TF_lowbound, TF_upbound){
-  TFs_cond <- (colSums(geneTF_matrix !=0) < TF_upbound*nrow(geneTF_matrix)) & (colSums(geneTF_matrix !=0) > TF_lowbound)
-  return(geneTF_matrix[,TFs_cond])
+filter_TF <- function(geneTF_prior, TF_lowbound, TF_upbound){
+  TFs_cond <- (colSums(geneTF_prior !=0) < TF_upbound*nrow(geneTF_prior)) & (colSums(geneTF_prior !=0) > TF_lowbound)
+  return(geneTF_prior[,TFs_cond])
 }
